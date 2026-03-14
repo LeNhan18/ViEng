eac0ad3e08ad7a1a80482b8686dd4a4b455a6043
LeThanhNhan <lnhan8132@gmail.com>
2026-03-14 22:59:40 +0700

    thiết kế giao diện

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import Exam from "./pages/Exam";
 import Result from "./pages/Result";
+import Translate from "./pages/Translate";
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/exam" element={<Exam />} />
         <Route path="/result" element={<Result />} />
+        <Route path="/translate" element={<Translate />} />
       </Route>
     </Routes>
   );
